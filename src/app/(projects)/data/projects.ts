22322411d049fa54a2e32bbabbf946706c49ed55
harsh <tyagiharsh607@gmail.com>
2025-01-03 03:03:26 +0530

    Added chat app

--- src/app/(projects)/data/projects.ts
+++ src/app/(projects)/data/projects.ts
@@ -5,11 +5,27 @@ import gym from "../../../../public/image/Gym app.png";
 import photon from "../../../../public/image/Photon.png";
 import wordle from "../../../../public/image/Wordle.png";
 import vote from "../../../../public/image/Vote Snap.png";
+import chatApp from "../../../../public/image/Chat-app.png";
 
 const data = {
   bestProjects: [
     {
       id: 1,
+      title: "E Commerce",
+      description:
+        "Elevate your online shopping journey with this responsive full-stack project. Featuring Home, Shop, Cart, Payment, Customized Search, and Authentication. E-com delivers a user-friendly experience.",
+      deployedUrl: "https://e-com-hk31.onrender.com/",
+      githubUrl: "https://github.com/tyagiharsh607/e-com-full-stack",
+      imgSrc: ecom,
+      skills: [
+        { id: 1, name: "React JS" },
+        { id: 2, name: "Node JS" },
+        { id: 3, name: "Express JS" },
+        { id: 4, name: "MongoDB" },
+      ],
+    },
+    {
+      id: 2,
       title: "Truthful Note",
       description:
         "Truthful Note is a Next.js application that allows users to receive anonymous messages for various purposes such as product reviews, feedback, confessions, and more. The platform offers a flexible messaging experience with customizable acceptance settings and robust user authentication.",
@@ -23,23 +39,24 @@ const data = {
       ],
     },
     {
-      id: 2,
-      title: "E Commerce",
+      id: 3,
+      title: "Full Stack Realtime Chat App",
       description:
-        "Elevate your online shopping journey with this responsive full-stack project. Featuring Home, Shop, Cart, Payment, Customized Search, and Authentication. E-com delivers a user-friendly experience.",
-      deployedUrl: "https://e-com-hk31.onrender.com/",
-      // deployedUrl: "https://e-com-mern.onrender.com/",
-      githubUrl: "https://github.com/tyagiharsh607/e-com-full-stack",
-      imgSrc: ecom,
+        "Full Stack Realtime Chat App is a MERN-based application enabling instant messaging with Socket.io, secure JWT authentication, and an intuitive UI built with TailwindCSS and Daisy UI. It supports online user status, Zustand for state management, and real-time scalability.",
+      deployedUrl: "https://chat-app-2qst.onrender.com",
+      githubUrl: "https://github.com/tyagiharsh607/chat-app",
+      imgSrc: chatApp,
       skills: [
-        { id: 1, name: "React JS" },
-        { id: 2, name: "Node JS" },
-        { id: 3, name: "Express JS" },
-        { id: 4, name: "MongoDB" },
+        { id: 1, name: "MongoDB" },
+        { id: 2, name: "Express.js" },
+        { id: 3, name: "React.js" },
+        { id: 4, name: "Node.js" },
+        { id: 5, name: "Socket.io" },
+        { id: 6, name: "Zustand" },
       ],
     },
     {
-      id: 3,
+      id: 4,
       title: "Vote Snap",
       description:
         "Vote Snap lets users vote on images in real-time, providing instant feedback on popular choices. It offers an interactive platform for image-based voting, engaging users with dynamic results.",
@@ -56,7 +73,7 @@ const data = {
       ],
     },
     {
-      id: 4,
+      id: 5,
       title: "Coin Ranking",
       description:
         "A web application that empowers users to access real-time information about various cryptocurrencies. The app offers features such as comparing cryptocurrency rankings, marketplace trends, assessing profit and loss data.",
@@ -69,7 +86,7 @@ const data = {
       ],
     },
     {
-      id: 5,
+      id: 6,
       title: "MyFitness Gym App",
       description:
         "A dynamic Gym App which utilizes cutting-edge technologies. Integrated external APIs to fetch a diverse range of exercises, enabling users to access comprehensive workout routines.",
@@ -82,7 +99,7 @@ const data = {
       ],
     },
     {
-      id: 6,
+      id: 7,
       title: "Photon",
       description:
         "Discover stunning images with Photon. Using the Pexels API, Photon instantly fetches and displays high-quality photos when you search by keyword. Explore and enjoy a vast library of professional-grade images with ease.",
@@ -97,7 +114,7 @@ const data = {
       ],
     },
     {
-      id: 7,
+      id: 8,
       title: "Wordle",
       description:
         "Wordle challenges players to guess a hidden word within six attempts. Each guess provides feedback on correct letters and their positions, incorrect letters, or letters not in the word, offering a fun and engaging word puzzle experience.",
